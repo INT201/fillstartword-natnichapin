ef55a35504babf5a8e2ba0dd3acd9f33a9aef16c
Natnicha Sirinipatkul <natnicha.pin@mail.kmutt.ac.th>
2022-09-01 23:54:20 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,10 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+    if(word===null ||  word===undefined){ return undefined }
+
+  if( word.startsWith(startWord) ){return word } 
+
+  return startWord.concat(word) 
 }
 module.exports = fillStartWord
